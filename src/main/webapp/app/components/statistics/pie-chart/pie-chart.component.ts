adbaa255a960733746513e5219f62f1f29bf0325
KaanHGunay <kaangunay16@gmail.com>
2021-02-12 23:31:56 +0300

    Pie Chart Added Label Added

--- src/main/webapp/app/components/statistics/pie-chart/pie-chart.component.ts
+++ src/main/webapp/app/components/statistics/pie-chart/pie-chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType } from 'chart.js';
+import 'chartjs-plugin-datalabels';
+import { Context } from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'jhi-pie-chart',
@@ -30,13 +32,38 @@ export class PieChartComponent implements OnInit {
       fullWidth: true,
       reverse: false,
     },
+
+    plugins: {
+      datalabels: {
+        display: true,
+        align: 'bottom',
+        backgroundColor: '#ccc',
+        borderRadius: 3,
+        font: {
+          size: 18,
+        },
+        formatter: this.formatterFunc,
+        color: '#fff',
+      },
+    },
   };
 
   constructor() {}
 
+  formatterFunc(value: number, ctx: Context): string {
+    let sum = 0;
+    const dataArr = ctx.dataset.data;
+
+    if (Array.isArray(dataArr)) {
+      dataArr.forEach((data: any) => {
+        sum += +data;
+      });
+    }
+    return ((100 * value) / sum).toFixed(2) + '%';
+  }
+
   chartFilled(): void {
     this.chartLabel.push('Red', 'Blue', 'Yellow', 'Gray', 'Green');
-
     this.chartData.push(12, 22, 32, 42, 52);
   }
 
